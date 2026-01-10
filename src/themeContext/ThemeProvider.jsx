47f1438ae042bdae2acc804fcb30cb435f892646
unknown <>
2026-01-10 16:45:05 +0600

    save changes

---
+++ src/themeContext/ThemeProvider.jsx
@@ -0,0 +1,23 @@
+import { ThemeContext } from "./ThemeContext";
+import { useEffect ,useState } from "react";
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => setTheme(prev => (prev === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
